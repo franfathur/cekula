9fc723b52d024beed3d3849a603c2d256e697681
franfathur <82429851+franfathur@users.noreply.github.com>
2022-12-06 13:17:19 +0700

    commit api login register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $req)
+    {
+        //valdiate
+        $rules = [
+            'name' => 'required|string',
+            'username' => 'required|string|unique:users',
+            'password' => 'required|string|min:6'
+        ];
+        $validator = Validator::make($req->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        //create new user in users table
+        $user = User::create([
+            'name' => $req->name,
+            'username' => $req->username,
+            'password' => Hash::make($req->password)
+        ]);
+        $token = $user->createToken('Personal Access Token')->plainTextToken;
+        $response = ['user' => $user, 'token' => $token];
+        return response()->json($response, 200);
+    }
+
+    public function login(Request $req)
+    {
+        // validate inputs
+        $rules = [
+            'username' => 'required',
+            'password' => 'required|string'
+        ];
+        $req->validate($rules);
+        // find user username in users table
+        $user = User::where('username', $req->username)->first();
+        // if user username found and password is correct
+        if ($user && Hash::check($req->password, $user->password)) {
+            $token = $user->createToken('Personal Access Token')->plainTextToken;
+            $response = ['user' => $user, 'token' => $token];
+            return response()->json($response, 200);
+        }
+        $response = ['message' => 'Incorrect username or password'];
+        return response()->json($response, 400);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\API\JadwalSekolahAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,12 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
-    
+
 // });
 // Route::middleware('auth:sanctum')->group(function(){
 //     Route::post('show', [JadwalSekolahAPIController::class, 'show']);
 //     Route::post('create', [JadwalSekolahAPIController::class, 'create']);
 // }) ;
 Route::get('jadwal-sekolah', [JadwalSekolahAPIController::class, 'index']);
-    
-    
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
